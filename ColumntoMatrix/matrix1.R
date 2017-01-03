1d440f602e886f05fc263d9299c9e177cce7002a
Francisco Jaramillo Aguilar <francisco.jaramilloaguilar@gmail.com>
2017-01-03 14:56:54 -0600

    prueba

---
+++ ColumntoMatrix/matrix1.R
@@ -0,0 +1,41 @@
+#install.packages("corrplot")
+
+library(corrplot)
+
+ar <- array(data = 0, dim = c(length(data$Numero), 5))
+
+for(m in 1:length(data$Numero)){
+  le = length(as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]]))
+  if(le == 5){
+    for(l in 1:5){
+      ar[m,l] <- as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]])[l]
+      }
+  }
+    if(le == 4){
+    for(l in 1:4){
+      ar[m,l+1] <- as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]])[l]
+    }
+  }
+  if(le == 3){
+    for(l in 1:3){
+      ar[m,l+2] <- as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]])[l]
+    }
+  }
+  if(le == 2){
+    for(l in 1:2){
+      ar[m,l+3] <- as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]])[l]
+    }
+  }
+  if(le == 1){
+    for(l in 1:1){
+      ar[m,l+4] <- as.numeric(strsplit(as.character(data$Numero[m]), "")[[1]])[l]
+    }
+  }
+}
+
+plot(ar[,5], type = 'o')
+hist(ar[,5])
+mean(ar[,5])
+
+c <- cor(ar)
+corrplot(c, method="number")
